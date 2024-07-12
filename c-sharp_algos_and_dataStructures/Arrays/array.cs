d72f35ceb2ef7319ee3532c0d94add25a9edd4a8
Judah Kahler <judah.kahler@gmail.com>
2024-07-12 15:14:53 -0500

    Array delete lesson

--- c-sharp_algos_and_dataStructures/Arrays/array.cs
+++ c-sharp_algos_and_dataStructures/Arrays/array.cs
@@ -6,6 +6,8 @@
 // How to insert at the end of an array.
 
 // Create the array.
+using System.Security.Principal;
+
 int[] intArray = new int[6];
 
 // Make a variable to keep track of the length.
@@ -45,3 +47,56 @@ for (int i = 4; i >= 2; i--)
 }
 
 intArray[2] = 8;
+
+// ==================================================================
+
+// How to Delete from the end of an array.
+
+int[] delIntArray = new int[9];
+
+int length = 0;
+
+for(int i = 0; i < 6; i++)
+{
+    delIntArray[length] = i;
+    length++;
+}
+
+// Here we reduce the length of the array effectivly deleting the last item.
+// length--;
+
+// ==================================================================
+
+// How to delete from the begining of the array.
+for(int i = 1; i<length; i++)
+{
+    delIntArray[i - 1] = delIntArray[i];
+}
+
+// We still reduce the length of the array because we have one item fewer.
+// length--;
+
+// ==================================================================
+
+// How to delete an item in the middle of the array.
+
+// The second number of the array gets deleted. ==> "int i = 2;"
+for(int i = 2; i < length; i++)
+{
+    delIntArray[i - 1] = delIntArray[i];
+}
+
+// Reduce the length.
+length--;
+
+
+// ==================================================================
+
+// Print the arraay.
+for(int i = 0; i < length; i++)
+{
+    Console.WriteLine(delIntArray[i]);
+}
+
+// ==================================================================
+
