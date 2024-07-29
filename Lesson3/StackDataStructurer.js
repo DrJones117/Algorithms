83f7010df4d89da46c9484b1d90042cb63a0ad51
Judah Kahler <judah.kahler@gmail.com>
2024-07-29 11:06:33 -0500

    Partial draft of lesson 3

---
+++ Lesson3/StackDataStructurer.js
@@ -0,0 +1,21 @@
+// First off, a Stack is a LIFO (last in, first out) data structure. Like a stack of plates or a stack of pancakes.
+// You can't rermove the bottom of a stack of pancakes without toppling the whole thing. 
+// You must remove the top one first which, consequently, was the last one you put in. Last in, First out.
+
+// We use push() to add to the top and pop() to remove from the top.
+
+class Main{
+    
+    constructor()
+    {
+        this.items = [];
+    }
+
+
+    // Stacks have five unique methods
+    // push(new item)
+    // pop()
+    // peek()
+    // isEmpty()
+    // printStack()
+}
