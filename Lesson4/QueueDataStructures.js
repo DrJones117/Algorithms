d45eef0213fb62ce4233543e6cb5ac8f634e248f
Judah Kahler <judah.kahler@gmail.com>
2024-07-31 13:34:19 -0500

    Draft Queue Data structures lesson

---
+++ Lesson4/QueueDataStructures.js
@@ -0,0 +1,7 @@
+// A Queue is a FIFO (First In First Out) data structure. 
+// It is a Linear Data Structure. It is designed to hold elements prior to proccessing.
+
+// A Queue has a Head and a Tail, and because it is a FIFO data struture, 
+// the head will be removed first and the item in the Queue will become the new head. This is called dequeuing.
+// Enqueuing adds an item to the back of the Queue and that new item becomes the new tail.
+
